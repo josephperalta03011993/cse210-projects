c37901ed58cf0bb023efdedcfd016f6a20c6ab16
josephperalta03011993 <joseph.peralta03011993@gmail.com>
2025-01-12 20:54:28 +0800

    working with sum

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        // Exercise 4
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
+        List<int> numbers = new List<int>();
+        int userInput = 1;
+
+        while (userInput != 0)
+        {
+            Console.Write("Enter number: ");
+            userInput = int.Parse(Console.ReadLine());
+
+            if (userInput != 0)
+            {
+                numbers.Add(userInput);
+            }
+        }
+
+        // Get the Sum
+        int sum = 0;
+        foreach (int item in numbers)
+        {
+            sum += item;
+        }
+        Console.WriteLine($"The sum is: {sum}");
     }
 }
