2b060026cbe2827ce87cf73aa82ce6ef7869da0c
josephperalta03011993 <joseph.peralta03011993@gmail.com>
2025-01-12 21:12:00 +0800

    finished week 1 exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -28,5 +28,39 @@ class Program
             sum += item;
         }
         Console.WriteLine($"The sum is: {sum}");
+
+        // Get the averate
+        float average = (float)sum / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        // Get the largest
+        int max = numbers[0];
+        foreach (int item in numbers)
+        {
+            if (item > max)
+            {
+                max = item;
+            }
+        }
+        Console.WriteLine($"The largest number is: {max}");
+
+        // Get the smallest positive number
+        int min = numbers[0];
+        foreach (int item in numbers)
+        {
+            if (item < min && item > 0)
+            {
+                min = item;
+            }
+        }
+        Console.WriteLine($"The smallest positive number is: {min}");
+
+        // Sort the list
+        numbers.Sort();
+        Console.WriteLine("The sorted list is: ");
+        foreach (int item in numbers)
+        {
+            Console.WriteLine($"{item} ");
+        }
     }
 }
