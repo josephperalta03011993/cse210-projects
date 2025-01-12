1875eaf23113c0391eff94842783b2dcf74c4ece
josephperalta03011993 <joseph.peralta03011993@gmail.com>
2025-01-12 20:19:40 +0800

    finished exercise 3 working game - Guess the magic number

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Loops
+        /*
+        C# provides four kinds of loops:
+            while
+            do-while
+            for
+            foreach
+        */
+
+        int magicNumber = new Random().Next(1, 100);
+        int guess = 0;
+        int attempts = 0;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is the magic number? ");
+            guess = int.Parse(Console.ReadLine());
+            attempts++;
+
+            if (guess > magicNumber) 
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
+        Console.WriteLine($"It took you {attempts} attempts to guess the magic number!");
+        Console.WriteLine("Do you want to play again? (yes/no)");
+        if (Console.ReadLine().ToLower() == "yes")
+        {
+            Main(args);
+        }
+        
     }
 }
