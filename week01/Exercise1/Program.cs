eac79ab116d7b87084fdc33c6800b36f95bb2105
josephperalta03011993 <joseph.peralta03011993@gmail.com>
2025-01-12 18:35:52 +0800

    finished week 1 exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // Ask the user for their first and last name
+        Console.Write("What is your first name? ");
+        string firstname = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastname = Console.ReadLine();
+
+        // Print the user's name
+        Console.Write($"Your name is {lastname}, {firstname} {lastname}.");
     }
 }
